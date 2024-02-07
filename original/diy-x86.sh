a509ade98328f444e876f997ac52032f1f71879f
skywrt <156793219+skywrt@users.noreply.github.com>
2024-02-07 13:58:28 +0800

    Create diy-x86.sh

---
+++ original/diy-x86.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-Selfuse.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
+
+# Add additional packages
+git clone -b main --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+curl -fsSL https://raw.githubusercontent.com/skywrt/OpenWrt/main/patch/Makefile > package/helloworld/luci-app-ssr-plus/Makefile
