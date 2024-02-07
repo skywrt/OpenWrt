a768d2c1c30ce539fe76c661434a302b47e3de53
skywrt <156793219+skywrt@users.noreply.github.com>
2024-02-07 14:27:00 +0800

    Update diy-x86.sh

--- original/diy-x86.sh
+++ original/diy-x86.sh
@@ -14,5 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # Add additional packages
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
